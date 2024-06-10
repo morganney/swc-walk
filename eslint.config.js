d7fc0f8318a733f861e8eff008aafd1d1b8e20ff
Morgan Ney <morganney@gmail.com>
2024-06-10 18:26:24 -0500

    chore: add style preferences as lint rules.

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,39 @@ export default tseslint.config(
           argsIgnorePattern: '^_',
         },
       ],
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'return',
+        },
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: ['do', 'for', 'if', 'try', 'switch', 'while'],
+        },
+        {
+          blankLine: 'always',
+          prev: ['do', 'for', 'if', 'try', 'switch', 'while'],
+          next: '*',
+        },
+        {
+          blankLine: 'always',
+          prev: ['let', 'const'],
+          next: '*',
+        },
+        {
+          blankLine: 'never',
+          prev: ['let', 'const'],
+          next: ['let', 'const'],
+        },
+        {
+          blankLine: 'any',
+          prev: 'cjs-import',
+          next: ['cjs-import', '*'],
+        },
+      ],
     },
   },
 )

110fead9d4c45bb7ca5060e57683ac0da11a56f3
maastrich <pinsaultm@gmail.com>
2024-06-17 12:08:47 +0200

    chore: Update npm dependency to latest stable version

--- eslint.config.js
+++ eslint.config.js
@@ -1,69 +1,69 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import nodePlugin from "eslint-plugin-n";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import nodePlugin from 'eslint-plugin-n'
 
 export default tseslint.config(
   eslint.configs.recommended,
-  nodePlugin.configs["flat/recommended"],
+  nodePlugin.configs['flat/recommended'],
   ...tseslint.configs.strictTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: "./tsconfig.meta.json",
+        project: './tsconfig.meta.json',
       },
     },
     rules: {
-      "no-console": "error",
-      "@typescript-eslint/no-use-before-define": "error",
-      "@typescript-eslint/no-duplicate-type-constituents": "error",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      'no-console': 'error',
+      '@typescript-eslint/no-use-before-define': 'error',
+      '@typescript-eslint/no-duplicate-type-constituents': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          argsIgnorePattern: "^_",
+          argsIgnorePattern: '^_',
         },
       ],
-      "padding-line-between-statements": [
-        "error",
+      'padding-line-between-statements': [
+        'error',
         {
-          blankLine: "always",
-          prev: "*",
-          next: "return",
+          blankLine: 'always',
+          prev: '*',
+          next: 'return',
         },
         {
-          blankLine: "always",
-          prev: "*",
-          next: ["do", "for", "if", "try", "switch", "while"],
+          blankLine: 'always',
+          prev: '*',
+          next: ['do', 'for', 'if', 'try', 'switch', 'while'],
         },
         {
-          blankLine: "always",
-          prev: ["do", "for", "if", "try", "switch", "while"],
-          next: "*",
+          blankLine: 'always',
+          prev: ['do', 'for', 'if', 'try', 'switch', 'while'],
+          next: '*',
         },
         {
-          blankLine: "always",
-          prev: ["let", "const"],
-          next: "*",
+          blankLine: 'always',
+          prev: ['let', 'const'],
+          next: '*',
         },
         {
-          blankLine: "never",
-          prev: ["let", "const"],
-          next: ["let", "const"],
+          blankLine: 'never',
+          prev: ['let', 'const'],
+          next: ['let', 'const'],
         },
         {
-          blankLine: "any",
-          prev: "cjs-import",
-          next: ["cjs-import", "*"],
+          blankLine: 'any',
+          prev: 'cjs-import',
+          next: ['cjs-import', '*'],
         },
       ],
     },
   },
   {
-    files: ["test/*"],
+    files: ['test/*'],
     rules: {
-      "@typescript-eslint/no-floating-promises": "off",
+      '@typescript-eslint/no-floating-promises': 'off',
     },
   },
   {
-    ignores: ["dist", "coverage", "eslint.config.js"],
-  }
-);
+    ignores: ['dist', 'coverage', 'eslint.config.js'],
+  },
+)
